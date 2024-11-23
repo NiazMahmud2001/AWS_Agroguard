76e7db8ea90ac215e0bc93736313497c9c3823eb
NiazMahmud2001 <niazmahmud500@gmail.com>
2024-11-24 03:15:07 +0400

    new added all

--- frontendLayer/proxy_folder/login_page/login_backend.php
+++ frontendLayer/proxy_folder/login_page/login_backend.php
@@ -2,10 +2,32 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://internal-apptier-lb-992887380.eu-north-1.elb.amazonaws.com/login_page/login_backend.php");
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST));
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($_POST)); // Forward POST data
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, true); # that fetch the headder in response 
 
 $response = curl_exec($ch);
+
+
+// Separate headers and body
+$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+$headers = substr($response, 0, $header_size);
+$body = substr($response, $header_size);
+
+// Forward the redirect if present
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+if ($http_code == 302 || $http_code == 301) {
+    preg_match('/Location: (.+)/', $headers, $matches);
+    if (isset($matches[1])) {
+        $redirect_url = trim($matches[1]);
+        header("Location: $redirect_url", true, $http_code);
+        exit;
+    }
+}else{
+    header("Location: http://WebTier-LB-1783077918.eu-north-1.elb.amazonaws.com");
+};
+
 curl_close($ch);
 
 echo $response;
