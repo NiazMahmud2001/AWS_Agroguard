80c930386c9e1a82e5bac095214126f3e5c74a30
NiazMahmud2001 <113389005+NiazMahmud2001@users.noreply.github.com>
2024-11-23 22:46:36 +0400

    Update backendOrderServices.php

--- test_backend_layer/myOrdersAndServices/backendOrderServices.php
+++ test_backend_layer/myOrdersAndServices/backendOrderServices.php
@@ -2,39 +2,9 @@
 
 <?php 
 
-
-    define("servername" , "rds-agroguard.ctkokooiid1w.eu-north-1.rds.amazonaws.com");
-    define("username" , "niazAdmin");
-    define("password" , "7835e6ef");
-    define("dbname" , "agroguard");
-
-    $Py_server = new mysqli(constant("servername") , constant("username"), constant("password"), constant("dbname"));
-
-    if (mysqli_error($Py_server)){
-    echo "Failed to connect to MySQL: " . $Py_server-> connect_error;
-    exit();
+    $arr = ["11111", "22222" , "33333", "444444", "5555555", "6666666"]; 
+    forrach($arr as $data){
+        echo "<h1> $data</h1><br>"; 
     };
-
-    $userNames = $_GET['ppName'];
-
-    $sql_command = "select droneid from userreg where `userName`='$userNames';";
-    $result_insert = $Py_server -> query($sql_command);
-    $rows = mysqli_fetch_row($result_insert);
-    $droneName  = explode("-" , $rows[0])[0];
-
-    $sqlCommandServices = "select serviceLink , serviceName from droneservices where `droneId`='$droneName';"; 
-    $result_insert = $Py_server -> query($sqlCommandServices);
-    $rows = mysqli_fetch_row($result_insert);
-
-    $t = 1;
-
-    $orders = [];
-    while ($rows = mysqli_fetch_row($result_insert)){
-        //echo "<a href='$rows[0]' class='servicesOptions'>$t : $rows[1] Link</a>";
-        $orders[] = "<a href='$rows[0]' class='servicesOptions'>$t : $rows[1] Link</a>";
-        $t++;
-    };
-    echo json_encode($orders);
-    $Py_server->close();
 
 ?>
