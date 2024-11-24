e93a0acbf8f2d33c5346243b99acc9916dd44d85
NiazMahmud2001 <113389005+NiazMahmud2001@users.noreply.github.com>
2024-11-24 20:15:51 +0400

    Update backendOrderServices.php

--- frontendLayer/proxy_folder/myOrdersAndServices/backendOrderServices.php
+++ frontendLayer/proxy_folder/myOrdersAndServices/backendOrderServices.php
@@ -1,19 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-<?php
-$userName = $_GET['userName'];  
-$backend_url = "http://internal-apptier-lb-992887380.eu-north-1.elb.amazonaws.com/myOrdersAndServices/backendOrderServices.php?ppName=$userName";
+        $userName = $_GET['userName'];  
+        $backend_url = "http://internal-apptier-lb-992887380.eu-north-1.elb.amazonaws.com/myOrdersAndServices/backendOrderServices.php?ppName=$userName";
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $backend_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $backend_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $response = curl_exec($ch);
 
-if(curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-} else {
-    echo $response;
-}
-curl_close($ch);
-?>
-
-
+        if(curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        } else {
+            header('Content-Type: application/json');
+            echo $response;
+        }
+        curl_close($ch);
+    
+</body>
+</html>
