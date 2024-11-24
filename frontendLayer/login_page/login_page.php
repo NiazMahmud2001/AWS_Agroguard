c41c70b83cc1a673d57bde30e570f4f7d514df07
NiazMahmud2001 <113389005+NiazMahmud2001@users.noreply.github.com>
2024-11-24 18:56:05 +0400

    Update login_page.php

--- frontendLayer/login_page/login_page.php
+++ frontendLayer/login_page/login_page.php
@@ -36,7 +36,8 @@
             }else{
                 $main_go = "../index.php";
             };
-            <a href=$main_go class="logo">
+            echo '<a href='.$main_go.' class="logo">';
+        ?>
               <svg
                 class="mother_logo"
                 width="440"
@@ -59,7 +60,6 @@
                 />
               </svg>
             </a>
-        ?>
 
         <div class="other_button">
           <?php
