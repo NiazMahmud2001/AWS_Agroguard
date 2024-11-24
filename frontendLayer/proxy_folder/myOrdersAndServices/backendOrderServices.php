0b38976bca0cd2afd71882245f9d24ef052c5a1b
NiazMahmud2001 <113389005+NiazMahmud2001@users.noreply.github.com>
2024-11-25 02:46:45 +0400

    Update backendOrderServices.php

--- frontendLayer/proxy_folder/myOrdersAndServices/backendOrderServices.php
+++ frontendLayer/proxy_folder/myOrdersAndServices/backendOrderServices.php
@@ -1,9 +1,9 @@
-
-    <?php
-        $userNames = $_GET['ppName'];  
+<?php
+        $userNames = $_GET['ppName'];
         $backend_url = "http://internal-apptier-lb-992887380.eu-north-1.elb.amazonaws.com/myOrdersAndServices/backendOrderServices.php?ppName=".$userNames;
 
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, $backend_url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
@@ -14,6 +14,8 @@
         } else {
             header('Content-Type: application/json');
             echo $response;
+
         }
         curl_close($ch);
-    ?>
+
+?>
